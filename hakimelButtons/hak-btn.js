bcdbc9ff76cf4740302c89f80e0bf64b80084dec
Maximilian Rudolph <rudolph-max@gmx.net>
2020-12-22 20:42:05 +0000

    Classifyed everything

--- hakimelButtons/hak-btn.js
+++ hakimelButtons/hak-btn.js
@@ -1,97 +1,138 @@
-let offset = 15;
+class HakBtn {
+
+	constructor(obj, state, disabled, width, height) {
+		this.obj = obj;
+		this.state = state;
+		this.disabled = disabled;
+		this.width = width;
+		this.height = height;
+		
+		this.offset = 15;
+		this.animationDuration = 250;
+		this.animationEasing = "cubic-bezier(.02,.42,.27,1)";
+
+		// Adding all the event listeners
+		obj.addEventListener("mouseenter", () => { this.overlayAnimationTrigger(event, this) });
+		obj.addEventListener("mouseleave", () => { this.overlayAnimationTrigger(event, this) });
+	}
+
+	fetchChildren() {
+		// Finding the buttons overlay and content in the DOM-Tree
+		Array.from(this.obj.childNodes).forEach( (entry) => {
+
+			if (entry.classList !== undefined) { // Element is not an empty '#text' element
+				if (entry.classList.contains('btn-hak-overlay')) {
+					this.overlay = entry;
+
+				} else if (entry.classList.contains('btn-hak-content')) {
+					this.content = entry;
+				}
+			}
+		});
+	}
+
+	setSizes() {
+		// Setting the different elements sizes (parent, content, overlay) depending on user-settings
+		if (this.obj.clientWidth != 0) { // Width has been set (by user / script)
+			// Elements get scaled (width) after obj 
+			this.overlay.style.width = this.obj.clientWidth + "px";
+			this.content.style.width = this.obj.clientWidth + "px";
+		} else {
+			// ELements get scaled (width) after content
+			this.obj.style.width = this.content.clientWidth + "px";
+			this.overlay.style.width = this.content.clientWidth + "px";
+		}
+
+		if (this.obj.clientHeight != 0) { // Height has been set (by user / script)
+			// Elements get scaled (height) after obj 
+			this.overlay.style.height = this.obj.clientHeight + "px";
+			this.content.style.height = this.obj.clientHeight + "px";
+		} else {
+			// ELements get scaled (height) after content
+			this.obj.style.height = this.content.clientHeight + "px";
+			this.overlay.style.height = this.content.clientHeight + "px";
+		}
+
+	}
+
+	animation(el, offsetX, offsetY, out) {
+		let keyframes, finishAction; 
+
+		if (!out) { // move in
+			keyframes = [
+				{ transform: "translate(" + offsetX + "px, " + offsetY +"px)", opacity: 0 },
+				{ transform: 'translate(0, 0)', opacity: 1 }
+			];
+			finishAction = function() {
+				el.overlay.style.transform = "translate(0, 0)";
+				el.overlay.style.opacity = 1;
+			}
+
+		} else { // move out
+			keyframes = [
+				{ transform: 'translate(0, 0)', opacity: 1 },
+				{ transform: "translate(" + offsetX + "px, " + offsetY +"px)", opacity: 0 }
+			];
+			finishAction = function() {
+				el.overlay.style.transform = "translate(0, 0)";
+				el.overlay.style.opacity = 0;
+			}
+		}
+
+		el.overlay.animate(keyframes, { duration: el.animationDuration, easing: el.animationEasing })
+		.onfinish = finishAction;
+	}
+
+	overlayAnimationTrigger(event, el) {
+		el.obj.classList.toggle('hover');
+
+		let rect = event.target.getBoundingClientRect();
+		let rectMouseX = event.clientX - rect.left; // X-Pos within element
+		let rectMouseY = event.clientY - rect.top; // X-Pos within element
+
+		let out = true;
+		if (event.type == "mouseenter") {
+			out = false;
+		}
+
+		let offsets;
+		if (rectMouseY > 2*rect.height / 3) { // lower third
+			if (rectMouseX > 2*rect.width / 3) { // right most third					
+				offsets = [1, 1];
+			} else if (rectMouseX > rect.width / 3) { // middle third
+				offsets = [0, 1];
+			} else { // left most third
+				offsets = [-1, 1];
+			}
+
+		} else if (rectMouseY > rect.height / 3) { // middle third
+			if (rectMouseX > 2*rect.width / 3) { // right most third
+				offsets = [1, 0];
+			} else { // left most third
+				offsets = [-1, 0];
+			}
+
+		} else { // upper third
+			if (rectMouseX > 2*rect.width / 3) { // right most third
+				offsets = [1, -1];
+			} else if (rectMouseX > rect.width / 3) { // middle third
+				offsets = [0, -1];
+			} else { // left most third
+				offsets = [-1, -1];
+			}
+		}
+
+		el.animation(el, el.offset*offsets[0], el.offset*offsets[1], out);
+	}
+
+}
 
 // Adding event-listeners to all buttons
 Array.from(document.getElementsByClassName("btn-hak")).forEach( (entry) => {
-		let overlay = entry.getElementsByClassName("btn-hak-overlay")[0];
-		let content = entry.getElementsByClassName("btn-hak-content")[0];
+	let state = entry.classList.contains('static') ? 'static' : 'dynamic';
+	let disabled = entry.classList.contains('disabled') ? true : false;
 
-		if (entry.style.width == "") {
-			entry.style.width = content.clientWidth + "px";
-		} else {
-			content.style.width = entry.style.width;
-		}
-		if (entry.style.height == "") {
-			entry.style.height = content.clientHeight + "px";
-		} else {
-			content.style.height = entry.style.height;
-		}
-
-		overlay.style.width = content.clientWidth + "px";
-		overlay.style.height = content.clientHeight + "px";
-
-	if (!entry.classList.contains('static') && !entry.classList.contains('disabled')) {
-		entry.addEventListener("mouseenter", overlayAnimationTrigger);
-		entry.addEventListener("mouseleave", overlayAnimationTrigger);
-	} 
+	let hakBtn = new HakBtn(entry, state, disabled, entry.clientWidth, entry.clientHeight);
+	hakBtn.fetchChildren();
+	hakBtn.setSizes();
 });
-
-function animate(element, offsetX, offsetY, out) {
-	if (!out) { // move in
-		keyframes = [
-			{ transform: "translate(" + offsetX + "px, " + offsetY +"px)", opacity: 0 },
-			{ transform: 'translate(0, 0)', opacity: 1 }
-		];
-		finishAction = function() {
-			element.style.transform = "translate(0, 0)";
-			element.style.opacity = 1;
-		}
-
-	} else { // move out
-		keyframes = [
-			{ transform: 'translate(0, 0)', opacity: 1 },
-			{ transform: "translate(" + offsetX + "px, " + offsetY +"px)", opacity: 0 }
-		];
-		finishAction = function() {
-			element.style.transform = "translate(0, 0)";
-			element.style.opacity = 0;
-		}
-	}
-
-	element.animate(keyframes, 
-		{
-			duration: 250,
-			easing: "cubic-bezier(.02,.42,.27,1)"
-		}
-	).onfinish = finishAction;
-}
-
-function overlayAnimationTrigger(event) {
-	this.classList.toggle('hover');
-
-	let overlay = this.getElementsByClassName("btn-hak-overlay")[0];
-
-	let rect = event.target.getBoundingClientRect();
-	let rectMouseX = event.clientX - rect.left; // X-Pos within element
-	let rectMouseY = event.clientY - rect.top; // X-Pos within element
-
-	let out = true;
-	if (event.type == "mouseenter") {
-		out = false;
-	}
-
-	if (rectMouseY > 2*rect.height / 3) { // lower third
-		if (rectMouseX > 2*rect.width / 3) { // right most third					
-			animate(overlay, offset, offset, out);
-		} else if (rectMouseX > rect.width / 3) { // middle third
-			animate(overlay, 0, offset, out);
-		} else { // left most third
-			animate(overlay, -offset, offset, out);
-		}
-
-	} else if (rectMouseY > rect.height / 3) { // middle third
-		if (rectMouseX > 2*rect.width / 3) { // right most third
-			animate(overlay, offset, 0, out);
-		} else { // left most third
-			animate(overlay, -offset, 0, out);
-		}
-
-	} else { // upper third
-		if (rectMouseX > 2*rect.width / 3) { // right most third
-			animate(overlay, offset, -offset, out);
-		} else if (rectMouseX > rect.width / 3) { // middle third
-			animate(overlay, 0, -offset, out);
-		} else { // left most third
-			animate(overlay, -offset, -offset, out);
-		}
-	}
-}
